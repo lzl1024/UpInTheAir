675cc64e3788891757d41a24bcb75ce19f4138fa
lzl1024 <zhuolinliu8@gmail.com>
2013-11-02 22:01:04 -0400

    static port

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -9,7 +9,8 @@ import javax.ws.rs.core.UriBuilder;
 public class Constants {
     public static DateFormat FORMAT = new SimpleDateFormat(
             "yyyy-MM-dd hh:mm:ss");
+    public static int port = 8080;
     public static final URI BASE_URI = UriBuilder.fromUri("http://localhost/")
-            .port(8080).build();
+            .port(port).build();
     public static String ANS_TITLE = "Up In The Air, 3929-1038-4476\n";
 }
