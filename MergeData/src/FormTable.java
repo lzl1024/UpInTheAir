b9b6bd260434d72c6073b9da5d790963418811ad
lzl1024 <zhuolinliu8@gmail.com>
2013-11-17 15:48:18 -0500

    form table

---
+++ MergeData/src/FormTable.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+/**
+ * 
+ * Get the table from origin csv file
+ * 
+ */
+public class FormTable {
+    public static class Table {
+        public long numOfTweet;
+        public ArrayList<Long> retweetList;
+
+        public Table() {
+            super();
+            this.numOfTweet = 0;
+            this.retweetList = new ArrayList<Long>();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String inputfile = "text_exported_0_2.csv";
+        String outputfile = "output.csv";
+
+        HashMap<Long, Table> userTable = new HashMap<Long, Table>();
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputfile));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            String[] element = line.split(",");
+            Long userid = Long.parseLong(element[0]);
+            Long retweet = Long.parseLong(element[1]);
+
+            // update number of tweets
+            Table row;
+            if (!userTable.containsKey(userid)) {
+                row = new Table();
+            } else {
+                row = userTable.get(userid);
+            }
+            row.numOfTweet++;
+            userTable.put(userid, row);
+
+            // update retweet list
+            if (retweet != -1L) {
+                if (!userTable.containsKey(retweet)) {
+                    row = new Table();
+                } else {
+                    row = userTable.get(retweet);
+                }
+                row.retweetList.add(userid);
+                userTable.put(retweet, row);
+            }
+        }
+
+        // output csv
+        PrintWriter writer = new PrintWriter(new FileWriter(outputfile));
+        for(Entry<Long, Table> entry : userTable.entrySet()) {
+            StringBuilder builder = new StringBuilder(entry.getKey().toString());
+            builder.append(",");
+            builder.append(entry.getValue().numOfTweet);
+            
+            for (Long retweet : entry.getValue().retweetList) {
+                builder.append(",");
+                builder.append(retweet);
+            }
+            
+            writer.println(builder.toString());
+        }
+        writer.flush();
+        writer.close();
+        reader.close();
+    }
+}
