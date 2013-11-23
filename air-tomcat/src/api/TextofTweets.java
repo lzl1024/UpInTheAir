62032a3d022873bd218ae23b98c093c85f8c1180
Yinsu Chu <chuyinsu@gmail.com>
2013-11-22 23:35:14 -0500

    Query 2, non-static table instance, static configuration instance.

--- air-tomcat/src/api/TextofTweets.java
+++ air-tomcat/src/api/TextofTweets.java
@@ -1,7 +1,5 @@
 package api;
 
-import java.util.Arrays;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,16 +8,38 @@ import javax.ws.rs.core.MediaType;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.ResultScanner;
-import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import util.Constants;
 
 @Path("q2")
 public class TextofTweets {
+
+	/* HBase table, column family and column */
+	private static final byte[] TABLE = Bytes.toBytes("tweettable");
+	private static final byte[] COLUMN_FAMILY = Bytes.toBytes("tweet");
+	private static final byte[] COLUMN = Bytes.toBytes("content");
+
+	private static Configuration config = null;
+
+	static {
+
+		/* set configurations */
+		config = HBaseConfiguration.create();
+		config.clear();
+
+		/* master IP address */
+		config.set("hbase.zookeeper.quorum", "54.205.169.96");
+		config.set("hbase.zookeeper.property.clientPort", "2181");
+
+		/* master DNS name */
+		config.set("hbase.master",
+				"ec2-54-205-169-96.compute-1.amazonaws.com:60000");
+	}
+
 	/**
 	 * TextofTweets request
 	 * 
@@ -28,45 +48,45 @@ public class TextofTweets {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String tweets(@QueryParam("time") String time) {
-		final byte[] COLUMN_FAMILY = Bytes.toBytes("tweet");
-		final byte[] COLUMN = Bytes.toBytes("content");
+
+		/* edge cases */
+		if (time == null) {
+			return null;
+		}
+
 		StringBuilder builder = new StringBuilder(Constants.ANS_TITLE);
 
-		Configuration config = HBaseConfiguration.create();
-		config.clear();
-		config.set("hbase.zookeeper.quorum",
-				"ec2-50-16-144-110.compute-1.amazonaws.com");
-		config.set("hbase.zookeeper.property.clientPort", "2181");
-		config.set("hbase.master",
-				"ec2-50-16-144-110.compute-1.amazonaws.com:60000");
 		HTable tweetTable = null;
-		Scan scan = null;
-		ResultScanner rs = null;
-		byte[] timeArray = Bytes.toBytes(time);
 		try {
-			tweetTable = new HTable(config, "tweettable");
-			scan = new Scan();
-			scan.addColumn(COLUMN_FAMILY, COLUMN);
-			rs = tweetTable.getScanner(scan);
-			for (Result r = rs.next(); r != null; r = rs.next()) {
-				if (Arrays.equals(r.getRow(), timeArray)) {
-					builder.append(new String(r.getValue(COLUMN_FAMILY, COLUMN)));
-					break;
-				}
-			}
+			tweetTable = new HTable(config, TABLE);
 		} catch (Exception ex) {
-			System.err.println("failed to retrieve result from hbase");
-		} finally {
-			if (rs != null) {
-				rs.close();
+			System.out.println("failed to connect to hbase");
+			return null;
+		}
+
+		Result result = null;
+		byte[] timeArray = Bytes.toBytes(time);
+		Get get = new Get(timeArray);
+
+		/* get tweet from hbase */
+		try {
+			result = tweetTable.get(get);
+		} catch (Exception ex) {
+			System.out.println("failed to retrieve result from hbase");
+			try {
+				tweetTable.close();
+			} catch (Exception nestedEx) {
+				System.out.println("failed to close connection to hbase");
 			}
-			if (tweetTable != null) {
-				try {
-					tweetTable.close();
-				} catch (Exception ex) {
-					System.err.println("failed to close hbase table");
-				}
-			}
+			return null;
+		}
+
+		builder.append(new String(result.getValue(COLUMN_FAMILY, COLUMN)));
+
+		try {
+			tweetTable.close();
+		} catch (Exception nestedEx) {
+			System.out.println("failed to close connection to hbase");
 		}
 
 		return builder.toString();
